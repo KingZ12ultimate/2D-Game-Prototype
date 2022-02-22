bebb94daed7d20d64e7a358ba8cfc55affe0f741
KingZ12ultimate <84376912+KingZ12ultimate@users.noreply.github.com>
2022-02-22 12:37:01 +0200

    Initalize Project

---
+++ New Prototype/Assets/Input/InputReader.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+[CreateAssetMenu(fileName = "New Input Reader", menuName = "Input/Reader")]
+public class InputReader : ScriptableObject, GameInput.IPlayerActions
+{
+    // Gameplay
+    public event UnityAction jumpEvent;
+    public event UnityAction jumpCanceledEvent;
+    public event UnityAction dashEvent;
+    public event UnityAction climbEvent;
+    public event UnityAction<Vector2> moveEvent;
+
+    private GameInput gameInput;
+
+    private void OnEnable()
+    {
+        if (gameInput == null)
+        {
+            gameInput = new GameInput();
+            gameInput.Player.SetCallbacks(this);
+        }
+
+        gameInput.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        gameInput.Player.Disable();
+    }
+
+    public void OnClimb(InputAction.CallbackContext context)
+    {
+        if (climbEvent != null && context.phase == InputActionPhase.Performed)
+        {
+            climbEvent.Invoke();
+        }
+    }
+
+    public void OnDash(InputAction.CallbackContext context)
+    {
+        if (dashEvent != null && context.phase == InputActionPhase.Performed)
+        {
+            dashEvent.Invoke();
+        }
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (jumpEvent != null && context.phase == InputActionPhase.Performed)
+        {
+            jumpEvent.Invoke();
+        }
+    }
+
+    public void OnJumpCancel(InputAction.CallbackContext context)
+    {
+        if (jumpCanceledEvent != null && context.phase == InputActionPhase.Performed)
+        {
+            jumpCanceledEvent.Invoke();
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if (moveEvent != null && context.phase == InputActionPhase.Performed)
+        {
+            moveEvent.Invoke(context.ReadValue<Vector2>());
+        }
+
+        if (moveEvent != null && context.phase == InputActionPhase.Canceled)
+        {
+            moveEvent.Invoke(Vector2.zero);
+        }
+    }
+}
---
+++ New Prototype/Assets/Scripts/States/Sub States/PlayerGlideState.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class PlayerGlideState : PlayerInAirState
+{
+    public PlayerGlideState(PlayerStateMachine player, StateMachine stateMachine, PlayerData data) : base(player, stateMachine, data)
+    {
+    }
+}
